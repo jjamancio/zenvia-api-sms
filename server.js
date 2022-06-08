23f95ef7b686a2b2689df58cac33033551840333
Joao Amancio <jjamancio@gmail.com>
2022-06-08 00:31:57 -0300

    last commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const { post } = require('request-promise');
+
+post({
+  uri: 'https://api.zenvia.com/v2/channels/sms/messages',
+  headers: {
+    'X-API-TOKEN': process.env.API_TOKEN
+  },
+  body: {
+    from: process.env.REMETENTE,
+    to: process.env.DESTINATARIO,
+    contents: [{
+      type: 'text',
+      text: process.env.MENSAGEM
+    }]
+  },
+  json: true
+})
+.then((response) => {
+  console.log('Response:', response);
+})
+.catch((error) => {
+  console.log('Error:', error);
+});
